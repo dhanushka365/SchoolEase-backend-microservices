00090d55a64a9f999e35a28e236271fd644ba6ed
dhanushka365 <technohub10000@gmail.com>
2023-03-03 21:31:28 +0530

    fraud client

--- clients/src/main/java/com/service/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/service/clients/fraud/FraudClient.java
@@ -1,13 +1,15 @@
 package com.service.clients.fraud;
 
-import com.service.clients.fraud.FraudCheckResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("fraud")
+@FeignClient(
+        name = "fraud",
+        url = "${clients.fraud.url}"
+)
 public interface FraudClient {
-    @GetMapping(path = "api/service/fraud-check/{studentId}")
+    @GetMapping(path = "api/v1/fraud-check/{studentId}")
     FraudCheckResponse isFraudster(
-            @PathVariable("studentId")  String studentId);
+            @PathVariable("studentId") Integer studentId);
     }
